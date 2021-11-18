7d0d72a549fce016914c17aec84f5ab5a0f82ce4
Hendi Santika <hendisantika@gmail.com>
2021-11-18 08:55:17 +0700

    #6 - Add MoneyDebitDTO

---
+++ src/main/java/com/hendisantika/dto/MoneyDebitDTO.java
@@ -0,0 +1,20 @@
+package com.hendisantika.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/21
+ * Time: 08.54
+ */
+@Data
+public class MoneyDebitDTO {
+    private double debitAmount;
+
+    private String currency;
+
+}
