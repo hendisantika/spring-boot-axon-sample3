7288401a68c242124ddd95131ce8b4e4e4c32b59
Hendi Santika <hendisantika@gmail.com>
2021-11-18 09:02:23 +0700

    #15 - Add Status Enum

---
+++ src/main/java/com/hendisantika/aggregate/Status.java
@@ -0,0 +1,14 @@
+package com.hendisantika.aggregate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/21
+ * Time: 09.02
+ */
+public enum Status {
+    CREATED, ACTIVATED, HOLD
+}
