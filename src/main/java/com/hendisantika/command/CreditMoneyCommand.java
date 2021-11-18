016674c70fff804cc57496f1494690eecfd2b7d8
Hendi Santika <hendisantika@gmail.com>
2021-11-18 09:00:18 +0700

    #13 - Add CreditMoneyCommand

---
+++ src/main/java/com/hendisantika/command/CreditMoneyCommand.java
@@ -0,0 +1,23 @@
+package com.hendisantika.command;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/21
+ * Time: 09.00
+ */
+public class CreditMoneyCommand extends BaseCommand<String> {
+
+    public final double creditAmount;
+
+    public final String currency;
+
+    public CreditMoneyCommand(String id, double creditAmount, String currency) {
+        super(id);
+        this.creditAmount = creditAmount;
+        this.currency = currency;
+    }
+}
