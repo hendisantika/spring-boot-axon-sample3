4b91a8c254b68180a5fc84c147ac4eda0e192707
Hendi Santika <hendisantika@gmail.com>
2021-11-18 09:05:18 +0700

    #19 - Add AccountHeldEvent

---
+++ src/main/java/com/hendisantika/event/AccountHeldEvent.java
@@ -0,0 +1,22 @@
+package com.hendisantika.event;
+
+import com.hendisantika.aggregate.Status;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/21
+ * Time: 09.04
+ */
+public class AccountHeldEvent extends BaseEvent<String> {
+
+    public final Status status;
+
+    public AccountHeldEvent(String id, Status status) {
+        super(id);
+        this.status = status;
+    }
+}
