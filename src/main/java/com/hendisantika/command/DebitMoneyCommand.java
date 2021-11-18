bae8c572cd2c02b7f80dc2d106f1ac52ad7d2b70
Hendi Santika <hendisantika@gmail.com>
2021-11-18 09:00:42 +0700

    #14 - Add DebitMoneyCommand

---
+++ src/main/java/com/hendisantika/command/DebitMoneyCommand.java
@@ -0,0 +1,23 @@
+package com.hendisantika.command;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/21
+ * Time: 09.00
+ */
+public class DebitMoneyCommand extends BaseCommand<String> {
+
+    public final double debitAmount;
+
+    public final String currency;
+
+    public DebitMoneyCommand(String id, double debitAmount, String currency) {
+        super(id);
+        this.debitAmount = debitAmount;
+        this.currency = currency;
+    }
+}
