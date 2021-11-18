8a9ed43f41222f12d4ae8681fb578623fc930ff7
Hendi Santika <hendisantika@gmail.com>
2021-11-18 08:54:44 +0700

    #5 - Add MoneyCreditDTO

---
+++ src/main/java/com/hendisantika/dto/MoneyCreditDTO.java
@@ -0,0 +1,19 @@
+package com.hendisantika.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/21
+ * Time: 08.54
+ */
+@Data
+public class MoneyCreditDTO {
+    private double creditAmount;
+
+    private String currency;
+}
