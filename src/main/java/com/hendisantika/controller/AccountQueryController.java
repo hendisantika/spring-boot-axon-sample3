7da43f539f61a557f3b61d37ce1ef07320e0d514
Hendi Santika <hendisantika@gmail.com>
2021-11-18 09:20:07 +0700

    #41 - Add getAccount API in AccountQueryController

--- src/main/java/com/hendisantika/controller/AccountQueryController.java
+++ src/main/java/com/hendisantika/controller/AccountQueryController.java
@@ -1,7 +1,10 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.AccountQueryEntity;
 import com.hendisantika.service.query.AccountQueryService;
 import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,10 @@ public class AccountQueryController {
 
     public AccountQueryController(AccountQueryService accountQueryService) {
         this.accountQueryService = accountQueryService;
+    }
+
+    @GetMapping("/{accountNumber}")
+    public AccountQueryEntity getAccount(@PathVariable(value = "accountNumber") String accountNumber) {
+        return accountQueryService.getAccount(accountNumber);
     }
 }
