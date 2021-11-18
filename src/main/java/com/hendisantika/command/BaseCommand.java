e378513778dfeda96247e5a4b45e6898f25d1187
Hendi Santika <hendisantika@gmail.com>
2021-11-18 08:59:27 +0700

    #11 - Add BaseCommand

---
+++ src/main/java/com/hendisantika/command/BaseCommand.java
@@ -0,0 +1,22 @@
+package com.hendisantika.command;
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/21
+ * Time: 08.59
+ */
+public class BaseCommand<T> {
+
+    @TargetAggregateIdentifier
+    public final T id;
+
+    public BaseCommand(T id) {
+        this.id = id;
+    }
+}
