ccc40dc1bbcf04471b4828150165617b1b0232c0
Hendi Santika <hendisantika@gmail.com>
2021-11-18 09:04:41 +0700

    #18 - Add AccountCreatedEvent

---
+++ src/main/java/com/hendisantika/event/AccountCreatedEvent.java
@@ -0,0 +1,23 @@
+package com.hendisantika.event;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/21
+ * Time: 09.04
+ */
+public class AccountCreatedEvent extends BaseEvent<String> {
+
+    public final double accountBalance;
+
+    public final String currency;
+
+    public AccountCreatedEvent(String id, double accountBalance, String currency) {
+        super(id);
+        this.accountBalance = accountBalance;
+        this.currency = currency;
+    }
+}
