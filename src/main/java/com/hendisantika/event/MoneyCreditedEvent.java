e3fbcca40f633889f1ed5ba12c589f858aed3f03
Hendi Santika <hendisantika@gmail.com>
2021-11-18 09:05:45 +0700

    #20 - Add MoneyCreditedEvent

---
+++ src/main/java/com/hendisantika/event/MoneyCreditedEvent.java
@@ -0,0 +1,23 @@
+package com.hendisantika.event;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/21
+ * Time: 09.05
+ */
+public class MoneyCreditedEvent extends BaseEvent<String> {
+
+    public final double creditAmount;
+
+    public final String currency;
+
+    public MoneyCreditedEvent(String id, double creditAmount, String currency) {
+        super(id);
+        this.creditAmount = creditAmount;
+        this.currency = currency;
+    }
+}
