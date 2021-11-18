047ce549bbf18da9b2019e0d99b9b3a8f2491b6e
Hendi Santika <hendisantika@gmail.com>
2021-11-18 09:17:02 +0700

    #33 - Add AccountCommandServiceImpl

---
+++ src/main/java/com/hendisantika/service/command/AccountCommandServiceImpl.java
@@ -0,0 +1,50 @@
+package com.hendisantika.service.command;
+
+import com.hendisantika.command.CreateAccountCommand;
+import com.hendisantika.command.CreditMoneyCommand;
+import com.hendisantika.command.DebitMoneyCommand;
+import com.hendisantika.dto.AccountCreateDTO;
+import com.hendisantika.dto.MoneyCreditDTO;
+import com.hendisantika.dto.MoneyDebitDTO;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/21
+ * Time: 09.16
+ */
+@Service
+public class AccountCommandServiceImpl implements AccountCommandService {
+
+    private final CommandGateway commandGateway;
+
+    public AccountCommandServiceImpl(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    @Override
+    public CompletableFuture<String> createAccount(AccountCreateDTO accountCreateDTO) {
+        return commandGateway.send(new CreateAccountCommand(UUID.randomUUID().toString(),
+                accountCreateDTO.getStartingBalance(), accountCreateDTO.getCurrency()));
+    }
+
+    @Override
+    public CompletableFuture<String> creditMoneyToAccount(String accountNumber, MoneyCreditDTO moneyCreditDTO) {
+        return commandGateway.send(new CreditMoneyCommand(accountNumber, moneyCreditDTO.getCreditAmount(),
+                moneyCreditDTO.getCurrency()));
+    }
+
+    @Override
+    public CompletableFuture<String> debitMoneyFromAccount(String accountNumber, MoneyDebitDTO moneyDebitDTO) {
+        return commandGateway.send(new DebitMoneyCommand(accountNumber, moneyDebitDTO.getDebitAmount(),
+                moneyDebitDTO.getCurrency()));
+    }
+}
